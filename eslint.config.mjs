234e65fe09a5daa29983aabeae33e1e2ccf7a3cb
hafij <hafij.alasad23@gmail.com>
2025-02-28 03:23:07 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: 'error',
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+]
