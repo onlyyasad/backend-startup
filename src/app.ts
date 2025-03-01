bf5e01889559c82bb584eed184e277847cb1bfba
hafij <hafij.alasad23@gmail.com>
2025-03-02 00:10:58 +0600

    feat: student model connected to route and successfully created a student into db

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/config/modules/student/student.route'
 
 const app = express()
 
 //parser
 app.use(express.json())
 app.use(cors())
+
+//application routes
+
+app.use('/api/v1/students', StudentRoutes)
 
 app.get('/', (req: Request, res: Response) => {
   const a = 'Hello World!'
