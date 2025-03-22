42a7711a7469afeb5b37f7ee1b54436150e9e228
hafij <hafij.alasad23@gmail.com>
2025-03-22 22:25:36 +0600

    fix: user id generation bug fixed

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -14,12 +14,26 @@ export const findLastStudentId = async () => {
     .sort({ createdAt: -1 })
     .lean()
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+  return lastStudent?.id ? lastStudent.id : undefined
 }
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
   //first time 0000
-  const currentId = (await findLastStudentId()) || (0).toString()
+  let currentId = (0).toString()
+
+  const lastStudentId = await findLastStudentId()
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6)
+  const lastStudentYear = lastStudentId?.substring(0, 4)
+  const currentSemesterCode = payload.code
+  const currentYear = payload.year
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6)
+  }
   let incrementId = String(Number(currentId) + 1).padStart(4, '0')
   incrementId = `${payload.year}${payload.code}${incrementId}`
 
