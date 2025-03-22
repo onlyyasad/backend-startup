49b7262a7e6328e29cbd9243c84be24a8c4ae492
hafij <hafij.alasad23@gmail.com>
2025-03-23 03:01:09 +0600

    feat: mongoose transaction used for deleting multiple doc at a time in mongodb

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from 'mongoose'
 import { Student } from './student.model'
+import AppError from '../../errors/appError'
+import { status as httpStatus } from 'http-status'
+import { User } from '../user/user.model'
 
 const getAllStudentFromDB = async () => {
   const result = await Student.find()
@@ -25,15 +29,44 @@ const getSingleStudentFromDB = async (id: string) => {
 }
 
 const deleteSingleStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true })
-    .populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicFaculty',
-      },
-    })
-    .populate('admissionSemester')
-  return result
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+
+    const deletedStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    )
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      })
+      .populate('admissionSemester')
+
+    if (!deletedStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student.')
+    }
+
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    )
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user.')
+    }
+
+    await session.commitTransaction()
+    await session.endSession()
+
+    return deletedStudent
+  } catch (_error) {
+    await session.abortTransaction()
+    await session.endSession()
+  }
 }
 
 export const StudentServices = {
