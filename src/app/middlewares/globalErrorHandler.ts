ab779d25ab44dd1ee2dff0a624f2a76f8144d3ce
hafij <hafij.alasad23@gmail.com>
2025-03-25 02:00:29 +0600

    feat: error re-assembled from zod error with our custom error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,19 +1,63 @@
-import { NextFunction, Request, Response } from 'express'
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import AppError from '../errors/appError'
+import { ZodError, ZodIssue } from 'zod'
+import { TErrorSources } from '../interface/error'
+import config from '../config'
 
-const globalErrorHandler = (
+/**
+ * Error Pattern:
+ * @param success : string
+ * @param message : string
+ * @param errorSources: {
+ * path: string,
+ * message: string
+ * }[]
+ * @param stack : string Note: only for dev environment, not for production
+ */
+
+const globalErrorHandler: ErrorRequestHandler = (
   error: AppError,
   req: Request,
   res: Response,
   _next: NextFunction,
 ) => {
-  const statusCode = error.statusCode || 500
-  const message = 'Something went wrong!'
+  let statusCode = error.statusCode || 500
+  let message = error.message || 'Something went wrong!'
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ]
+
+  const handleZodError = (error: ZodError) => {
+    const statusCode = 400
+
+    const errorSources: TErrorSources = error.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      }
+    })
+    return {
+      statusCode,
+      message: 'Zod Validation Error',
+      errorSources,
+    }
+  }
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorSources = simplifiedError.errorSources
+  }
 
   res.status(statusCode).json({
     success: false,
-    message: error.message || message,
-    error,
+    message: message,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? error?.stack : null,
   })
 }
 
