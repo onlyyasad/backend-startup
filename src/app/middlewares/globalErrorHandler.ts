06811a82745245ab47a885c11afac656d89f15f2
hafij <hafij.alasad23@gmail.com>
2025-03-25 02:21:57 +0600

    feat: converted mongoose error to our custom error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,8 +1,10 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import AppError from '../errors/appError'
-import { ZodError, ZodIssue } from 'zod'
+import { ZodError } from 'zod'
 import { TErrorSources } from '../interface/error'
 import config from '../config'
+import handleZodError from '../errors/handleZodError'
+import handleValidationError from '../errors/handleValidationError'
 
 /**
  * Error Pattern:
@@ -15,12 +17,7 @@ import config from '../config'
  * @param stack : string Note: only for dev environment, not for production
  */
 
-const globalErrorHandler: ErrorRequestHandler = (
-  error: AppError,
-  req: Request,
-  res: Response,
-  _next: NextFunction,
-) => {
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, _next) => {
   let statusCode = error.statusCode || 500
   let message = error.message || 'Something went wrong!'
   let errorSources: TErrorSources = [
@@ -30,24 +27,13 @@ const globalErrorHandler: ErrorRequestHandler = (
     },
   ]
 
-  const handleZodError = (error: ZodError) => {
-    const statusCode = 400
-
-    const errorSources: TErrorSources = error.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue.path[issue.path.length - 1],
-        message: issue.message,
-      }
-    })
-    return {
-      statusCode,
-      message: 'Zod Validation Error',
-      errorSources,
-    }
-  }
-
   if (error instanceof ZodError) {
     const simplifiedError = handleZodError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorSources = simplifiedError.errorSources
+  } else if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorSources = simplifiedError.errorSources
