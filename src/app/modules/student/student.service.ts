5bb158a561b1ef7067fcd1381c4e47a3d5caa912
hafij <hafij.alasad23@gmail.com>
2025-03-27 01:52:15 +0600

    feat: query chaining

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -6,16 +6,24 @@ import { User } from '../user/user.model'
 import { TStudent } from './student.interface'
 
 const getAllStudentFromDB = async (query: Record<string, unknown>) => {
+  const queryObj = { ...query }
+  const studentSearchableFields = ['email', 'name.firstName', 'presentAddress']
   let searchTerm = ''
   if (query.searchTerm) {
     searchTerm = query.searchTerm as string
   }
 
-  const result = await Student.find({
-    $or: ['email', 'name.firstName', 'presentAddress'].map((field) => ({
+  const searchQuery = Student.find({
+    $or: studentSearchableFields.map((field) => ({
       [field]: { $regex: searchTerm, $options: 'i' },
     })),
   })
+
+  //filtering
+  const excludeFields = ['searchTerm', 'sort', 'limit']
+  excludeFields.forEach((el) => delete queryObj[el])
+  const filterQuery = searchQuery
+    .find(queryObj)
     .populate({
       path: 'academicDepartment',
       populate: {
@@ -23,7 +31,21 @@ const getAllStudentFromDB = async (query: Record<string, unknown>) => {
       },
     })
     .populate('admissionSemester')
-  return result
+
+  let sort = ''
+  if (query.sort) {
+    sort = query.sort as string
+  }
+
+  const sortQuery = filterQuery.sort(sort)
+
+  let limit = 1
+  if (query.limit) {
+    limit = query.limit
+  }
+
+  const limitQuery = await sortQuery.limit(limit)
+  return limitQuery
 }
 
 const getSingleStudentFromDB = async (id: string) => {
