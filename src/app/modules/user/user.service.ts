9dcc5995a054775467d632113c2863c9d65ca6c0
hafij <hafij.alasad23@gmail.com>
2025-04-15 23:43:20 +0600

    feat: create faculty under user service method initialized

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -60,6 +60,58 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
   }
 }
 
+const createFacultyIntoDB = async (password: string, payload: TStudent) => {
+  //create a user object
+  const userData: Partial<TUser> = {}
+
+  userData.password = password || (config.default_password as string)
+
+  //set student role
+  userData.role = 'student'
+
+  //set manually generated id
+
+  const admissionSemester = await AcademicSemester.findById(
+    payload.admissionSemester,
+  )
+
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+    userData.id = await generateStudentId(admissionSemester)
+
+    //create a user (---- Transaction 1 -----)
+    const newUser = await User.create([userData], { session })
+
+    // create a student
+
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user.')
+    }
+    //set id, _id as user
+    payload.id = newUser[0].id //embedding id
+    payload.user = newUser[0]._id //reference id
+
+    //create a student (---- Transaction 2 -----)
+
+    const newStudent = await Student.create([payload], { session })
+
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student.')
+    }
+
+    await session.commitTransaction()
+    await session.endSession()
+
+    return newStudent[0]
+  } catch (_error) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student.')
+  }
+}
+
 export const UserServices = {
   createStudentIntoDB,
+  createFacultyIntoDB,
 }
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -17,15 +17,15 @@ export const findLastStudentId = async () => {
   return lastStudent?.id ? lastStudent.id : undefined
 }
 
-export const generateStudentId = async (payload: TAcademicSemester) => {
+export const generateStudentId = async (payload: TAcademicSemester | null) => {
   //first time 0000
   let currentId = (0).toString()
 
   const lastStudentId = await findLastStudentId()
   const lastStudentSemesterCode = lastStudentId?.substring(4, 6)
   const lastStudentYear = lastStudentId?.substring(0, 4)
-  const currentSemesterCode = payload.code
-  const currentYear = payload.year
+  const currentSemesterCode = payload?.code
+  const currentYear = payload?.year
 
   if (
     lastStudentId &&
@@ -35,7 +35,7 @@ export const generateStudentId = async (payload: TAcademicSemester) => {
     currentId = lastStudentId.substring(6)
   }
   let incrementId = String(Number(currentId) + 1).padStart(4, '0')
-  incrementId = `${payload.year}${payload.code}${incrementId}`
+  incrementId = `${payload?.year}${payload?.code}${incrementId}`
 
   return incrementId
 }
