86efbe84212102da139a5ab2a205408ad9276e6b
hafij <hafij.alasad23@gmail.com>
2025-04-16 00:59:45 +0600

    feat: faculty schema and model created

---
+++ src/app/modules/faculty/faculty.model.ts
@@ -0,0 +1,145 @@
+import { Schema, model } from 'mongoose'
+import validator from 'validator'
+import { TFaculty, TFacultyModel, TUserName } from './faculty.interface'
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    minlength: [4, 'First Name must be at least 4 characters long.'],
+    maxlength: [20, 'First Name is too long.'],
+    required: [true, 'First name is required'],
+    trim: true,
+    validate: {
+      validator: function (value: string) {
+        const char = value.charAt(0).toUpperCase()
+        const rest = value.slice(1).toLowerCase()
+        const name = char + rest
+        return value === name
+      },
+      message: '{VALUE} is not valid.',
+    },
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    validate: {
+      validator: (value: string) => {
+        return validator.isAlpha(value)
+      },
+      message: '{VALUE} is not valid.',
+    },
+    required: [true, 'Last name is required'],
+  },
+})
+
+const facultySchema = new Schema<TFaculty, TFacultyModel>(
+  {
+    id: {
+      type: String,
+      unique: true,
+      required: [true, 'Student ID is required'],
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'User id is required.'],
+      unique: true,
+      ref: 'User',
+    },
+    name: {
+      type: userNameSchema,
+      required: [true, 'Student name is required'],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ['male', 'female', 'other'],
+        message:
+          "{VALUE} is not supported, use 'male', 'female' or 'other' instead",
+      },
+      required: [true, 'Gender is required'],
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      validate: {
+        validator: (value: string) => {
+          return validator.isEmail(value)
+        },
+        message: '{VALUE} is not a valid email.',
+      },
+      unique: true,
+    },
+    contactNo: {
+      type: String,
+      required: [true, 'Contact number is required'],
+    },
+    emergencyContactNo: {
+      type: String,
+      required: [true, 'Emergency contact number is required'],
+    },
+
+    presentAddress: {
+      type: String,
+      required: [true, 'Present address is required'],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, 'Permanent address is required'],
+    },
+    profileImg: {
+      type: String,
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicDepartment',
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+  },
+)
+
+facultySchema.virtual('fullName').get(function () {
+  return (
+    this?.name?.firstName +
+    ' ' +
+    this?.name?.middleName +
+    ' ' +
+    this?.name?.lastName
+  )
+})
+
+facultySchema.statics.isUserExists = async function (id: string) {
+  const existingUser = await Faculty.findOne({ id })
+  return existingUser
+}
+
+facultySchema.pre('findOne', async function (next) {
+  this.findOne({ isDeleted: { $ne: true } })
+  next()
+})
+
+facultySchema.pre('find', async function (next) {
+  this.find({ isDeleted: { $ne: true } })
+  next()
+})
+
+facultySchema.pre('aggregate', async function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } })
+  next()
+})
+
+export const Faculty = model<TFaculty, TFacultyModel>('Faculty', facultySchema)
