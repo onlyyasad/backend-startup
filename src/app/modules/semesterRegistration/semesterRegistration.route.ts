5a462825df2439b02957c111440d1dd42fd7b560
hafij <hafij.alasad23@gmail.com>
2025-10-22 00:18:33 +0600

    feat: semester registration routes initialized

---
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import validateRequest from '../../middlewares/validateRequest'
+import { SemesterRegistrationValidation } from './semesterRegistration.validation'
+import { SemesterRegistrationController } from './semesterRegistration.controller'
+
+const router = Router()
+
+router.post(
+  '/create-semester-registration',
+  validateRequest(
+    SemesterRegistrationValidation.createSemesterRegistrationValidationSchema,
+  ),
+  SemesterRegistrationController.createSemesterRegistration,
+)
+
+export const SemesterRegistrationRoutes = router
