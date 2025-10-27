83ea2ee73ae93c9ed336d830e8641bc74ea30caf
hafij <hafij.alasad23@gmail.com>
2025-10-27 23:56:11 +0600

    feat: offered course validation created

---
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+import { Days } from './offeredCourse.constant'
+
+const createOfferedCourseValidationSchema = z.object({
+  body: z.object({
+    semesterRegistration: z.string(),
+    academicSemester: z.string(),
+    academicFaculty: z.string(),
+    academicDepartment: z.string(),
+    course: z.string(),
+    faculty: z.string(),
+    maxCapacity: z.number(),
+    section: z.number(),
+    days: z.enum(Days as [string, ...string[]]),
+    startTime: z.string(),
+    endTime: z.string(),
+  }),
+})
+
+export const OfferedCourseValidation = {
+  createOfferedCourseValidationSchema,
+}
