fbe64ccad8ec1965886bcbd5a8a5b45bba48e159
hafij <hafij.alasad23@gmail.com>
2025-11-17 23:26:00 +0600

    feat: logged in user with validation

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,8 +1,33 @@
+import AppError from '../../errors/appError'
+import { User } from '../user/user.model'
 import { TLoginUser } from './auth.interface'
+import { status as httpStatus } from 'http-status'
+import bcrypt from 'bcrypt'
 
 const loginUserInDB = async (payload: TLoginUser) => {
-  // Implementation for user login
-  console.log('Logging', payload)
+  const isUserExists = await User.findOne({ id: payload?.id })
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found.')
+  }
+
+  if (isUserExists.isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is deleted.')
+  }
+
+  if (isUserExists.status === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is blocked.')
+  }
+
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    isUserExists.password,
+  )
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Password is incorrect.')
+  }
+
+  console.log(isUserExists)
 }
 
 export const AuthService = {
