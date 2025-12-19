a0b1ec9a5c950f8764141d94e281d77775fd0aeb
hafij <hafij.alasad23@gmail.com>
2025-12-20 03:20:31 +0600

    feat: auth guard added to semester registration routes

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,23 +2,45 @@ import express from 'express'
 import { AcademicSemesterControllers } from './academicSemester.controller'
 import validateRequest from '../../middlewares/validateRequest'
 import { AcademicSemesterValidation } from './academicSemester.validation'
+import auth from '../../middlewares/auth'
+import { USER_ROLE } from '../user/user.constant'
 
 const router = express.Router()
 
 router.post(
   '/create-academic-semester',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicSemesterValidation.createAcademicSemesterValidationSchema,
   ),
   AcademicSemesterControllers.createAcademicSemester,
 )
 
-router.get('/', AcademicSemesterControllers.getAcademicSemesters)
+router.get(
+  '/',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  AcademicSemesterControllers.getAcademicSemesters,
+)
 
-router.get('/:courseId', AcademicSemesterControllers.getSingleAcademicSemester)
+router.get(
+  '/:courseId',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  AcademicSemesterControllers.getSingleAcademicSemester,
+)
 
 router.patch(
   '/:courseId',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
     AcademicSemesterValidation.updateAcademicSemesterValidationSchema,
   ),
