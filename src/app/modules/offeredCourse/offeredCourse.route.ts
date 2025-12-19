0b72329b50cd5e4cd46aa9bcccdb544f3f2fc0c4
hafij <hafij.alasad23@gmail.com>
2025-12-20 04:08:06 +0600

    feat: auth guard for offered course

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import { OfferedCourseController } from './offeredCourse.controller'
 import validateRequest from '../../middlewares/validateRequest'
 import { OfferedCourseValidation } from './offeredCourse.validation'
+import auth from '../../middlewares/auth'
+import { USER_ROLE } from '../user/user.constant'
 
 const router = Router()
 
@@ -9,15 +11,21 @@ router.get('/', OfferedCourseController.getAllOfferedCourses)
 router.get('/:id', OfferedCourseController.getSingleOfferedCourse)
 router.post(
   '/create-offered-course',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(OfferedCourseValidation.createOfferedCourseValidationSchema),
   OfferedCourseController.createOfferedCourse,
 )
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(OfferedCourseValidation.updateOfferedCourseValidationSchema),
   OfferedCourseController.updateOfferedCourse,
 )
 
-router.delete('/:id', OfferedCourseController.deleteOfferedCourse)
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  OfferedCourseController.deleteOfferedCourse,
+)
 
 export const OfferedCourseRoutes = router
